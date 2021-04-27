65b4a9190445eb5847a0723572352528281bd78f
Lilly Vernon <littletomoya@yahoo.com>
2021-04-26 23:41:14 -0500

    ...

--- app/config.py
+++ app/config.py
@@ -5,8 +5,10 @@ class Config(object):
     DEBUG = False
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'Som3$ec5etK*y'
     UPLOAD_FOLDER = './app/static/uploads'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://finalproject:lilly@localhost/finalproject'
+    #SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://finalproject:lilly@localhost/finalproject'
+    DATABASE_URL='postgres://mxijebqfvcprxh:fa3bf93a3e526e04a34f7e8a9d2b825a56d8b5646352958215a93e8e0485a586@ec2-34-206-8-52.compute-1.amazonaws.com:5432/d7qphv8v5jdgqp'
     SQLALCHEMY_TRACK_MODIFICATIONS = False 
+    
     #set SECRET_KEY=YourRandomSecretKey
     #set DATABASE_URL=postgresql://finalproject:lilly@localhost/finalproject
 class DevelopmentConfig(Config):
